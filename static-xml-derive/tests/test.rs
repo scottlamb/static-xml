54f671389d192721aed7dc5ba172ba686ad425a8
Scott Lamb <slamb@slamb.org>
2022-10-23 14:21:47 -0700

    add missing copyright headers

--- static-xml-derive/tests/test.rs
+++ static-xml-derive/tests/test.rs
@@ -1,3 +1,6 @@
+// Copyright (C) 2022 Scott Lamb <slamb@slamb.org>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
 #[test]
 fn test() {
     let t = trybuild::TestCases::new();
--- static-xml-derive/tests/ui/field-not-parsetext.rs
+++ static-xml-derive/tests/ui/field-not-parsetext.rs
@@ -1,3 +1,6 @@
+// Copyright (C) 2022 Scott Lamb <slamb@slamb.org>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
 use static_xml_derive::Deserialize;
 
 struct Foo;
